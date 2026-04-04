e18fddc41d6599b4ada6170dba915b3ab2ee140d
F18-Maverick <xu_ruihong2009@163.com>
2026-04-04 19:07:04 +0800

    core-code: fix the class name error in tcp protocal test files

--- test/test_TCP_server.py
+++ test/test_TCP_server.py
@@ -5,5 +5,5 @@ if package_dictionary not in os.sys.path:
     sys.path.insert(0, package_dictionary)
 from src import connect_tcp
 def test_TCP_server():
-    connect_tcp.TCPServer_Base()
+    connect_tcp.TCP_Server_Base()
 test_TCP_server()
